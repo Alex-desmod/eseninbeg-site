382498582b6f4a0f8610679e360d4d7c78fed6a3
Alex Gusev <alex.desmod@gmail.com>
2026-07-07 14:16:58 +0300

    base config done, settings are split into base.py, development.py and production.py modules

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,24 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z4t3x=m9(@_x%oa_@tvp9qvvgh#ef_-mg=3+1t4pdfgkds8u-_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# SECRET_KEY = 'django-insecure-z4t3x=m9(@_x%oa_@tvp9qvvgh#ef_-mg=3+1t4pdfgkds8u-_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Application definition
@@ -75,10 +74,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
---
+++ config/settings/production.py
@@ -0,0 +1,5 @@
+import os
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
