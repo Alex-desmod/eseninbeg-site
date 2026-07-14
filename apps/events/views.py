6dadd2d260f8b0d72d7ffdefc965839235833ce5
Alex Gusev <alex.desmod@gmail.com>
2026-07-14 23:34:43 +0300

    event_detail.html in the making

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Event
+from .models import Event, DistanceRecord
+
 
 # Create your views here.
 class EventDetailView(DetailView):
@@ -9,3 +10,10 @@ class EventDetailView(DetailView):
     context_object_name = 'event'
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['records'] = DistanceRecord.objects.filter(
+            distance__event=self.object
+        ).select_related('distance')
+        return context
